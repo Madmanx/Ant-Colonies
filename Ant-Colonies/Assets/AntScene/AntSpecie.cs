f8e698246989283e1e92aa4c8c37a6d4ef240f42
Unknown <remi.boscher@gmail.com>
2018-06-07 02:40:59 +0200

    Adding draw call minimizer

---
+++ Ant-Colonies/Assets/AntScene/AntSpecie.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AntSpecie", menuName = "Custom/AntSpecie", order = 2)]
+[System.Serializable]
+public class AntSpecie : ScriptableObject {
+
+    [Header("Head")]
+    [SerializeField]
+    [Range(0.8f, 1.2f)]
+    public float headSize = 1.0f;
+    [SerializeField]
+    public Color headColor;
+    [SerializeField]
+    public GameObject Head;
+
+    [Header("Thorax")]
+    [SerializeField]
+    [Range(0.8f, 1.2f)]
+    public float thoraxSize = 1.0f;
+    [SerializeField]
+    public Color thoraxColor;
+    [SerializeField]
+    public GameObject Thorax;
+
+    [Header("Abdomen")]
+    [SerializeField]
+    [Range(0.8f, 1.2f)]
+    public float abdomenSize = 1.0f;
+    [SerializeField]
+    public Color abdomenColor;
+    [SerializeField]
+    public GameObject Abdomen;
+}
